59316523b0a4c0b0ce761239d4e5b9acfbc4ddf6
Thomas Johnson <tjohnson1106@gmail.com>
2017-08-23 18:04:32 -0400

    added seed.js object array file and passed to ProductList component

---
+++ public/js/seed.js
@@ -0,0 +1,12 @@
+const products = [
+  {
+    id: 1,
+    title: 'Yellow Pail',
+    description: 'On-demand sand castle construction expertise'
+    url: '#',
+    votes: generateVoteCount(),
+    submitterAvatarUrl: 'images/avatars/daniel.jpg',
+    productImageUrl: 'images/products/image-aqua.png',
+
+  },
+]
