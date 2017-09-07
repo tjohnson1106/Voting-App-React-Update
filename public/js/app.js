1d372a5f2e81dbe77d5d6f7802ebb3c029e122e6
Thomas Johnson <tjohnson1106@gmail.com>
2017-09-07 14:31:56 -0400

    added product component array

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,63 @@
+
 class ProductList extends React.Component {
-    render() {
-        const product = Seed.products[0];
+  render() {
+    const productComponents = Seed.products.map((product) => (
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+      />
+    ));
+    return (
+      <div className='ui unstackable items'>
+        {productComponents}
+      </div>
+    );
+  }
+}
+
+
+    class Product extends React.Component {
+      render() {
         return (
-            <div className='ui unstackable items'>
-                <Product 
-                id={product.id}
-                title={product.title}
-                description={product.description}
-                url={product.url}
-                votes={product.votes}
-                submitterAvatarUrl={product.submitterAvatarUrl}
-                productImageUrl={product.productImageUrl}
+          <div className='item'>
+            <div className='image'>
+              <img src={this.props.productImageUrl} />
+            </div>
+            <div className='middle aligned content'>
+              <div className='header'>
+                <a>
+                  <i className='large caret up icon' />
+                </a>
+                {this.props.votes}
+              </div>
+              <div className='description'>
+                <a href={this.props.url}>
+                  {this.props.title}
+                </a>
+                <p>
+                  {this.props.description}
+                </p>
+              </div>
+              <div className='extra'>
+                <span>Submitted by:</span>
+                <img
+                  className='ui avatar image'
+                  src={this.props.submitterAvatarUrl}
                 />
+              </div>
             </div>
+          </div>
         );
+      }
     }
-}
 
-class Product extends React.Component {
-    render() {
-        return (
-            <div className='item'>
-                <div className='image'>
-                    <img src='images/products/image-aqua.png' />
-                </div>
-                <div className='middle aligned content'>
-                    <div className='description'>
-                        <a>Fort Knight</a>
-                        <p>Authentic renaissance actors, delivered in just two weeks.</p>
-                    </div>
-                    <div className='extra'>
-                        <span>Submitted by:</span>
-                        <img
-                            className='ui avatar image'
-                            src='images/avatars/daniel.jpg'
-                        />
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(
-    <ProductList />,
-    document.getElementById('content')
-);
+    ReactDOM.render(
+      <ProductList />,
+      document.getElementById('content')
+    );
--- public/js/seed.js
+++ public/js/seed.js
@@ -1,12 +1,19 @@
-const products = [
-  {
-    id: 1,
-    title: 'Yellow Pail',
-    description: 'On-demand sand castle construction expertise'
-    url: '#',
-    votes: generateVoteCount(),
-    submitterAvatarUrl: 'images/avatars/daniel.jpg',
-    productImageUrl: 'images/products/image-aqua.png',
+window.Seed = (function () {
+  function generateVoteCount() {
+    return Math.floor((Math.random() * 50) + 15);
+  }
 
-  },
-]
+  const products = [
+    {
+      id: 1,
+      title: 'Yellow Pail',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: 'images/avatars/daniel.jpg',
+      productImageUrl: 'images/products/image-aqua.png',
+    },
+  ];
+
+  return { products: products };
+}());
